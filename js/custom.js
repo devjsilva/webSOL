8a05e4e3fd2741ce499b3864fbd755f84338cf08
DamienVaz <vaz.damien@gmail.com>
2014-11-20 20:18:32 +0000

    trying to fix responsive for mobile

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ $(function() {
 	});
 
 	// Backstretchs
-	$("#body").backstretch("images/3.jpg"); 
+	$("#header").backstretch("images/3.jpg"); 
 	//$("#services").backstretch("images/3.jpg");
 	
 	// Countdown
